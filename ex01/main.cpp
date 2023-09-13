8311965ad9f34340f67780b538c0faccd34a8f83
Gabriel de Jesus <devgabrieldejesus@gmail.com>
2023-09-13 16:58:53 -0300

    fix: fix commands

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gde-jesu <gde-jesu@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/01 18:37:43 by gde-jesu          #+#    #+#             */
-/*   Updated: 2023/09/09 20:24:23 by gde-jesu         ###   ########.fr       */
+/*   Updated: 2023/09/13 16:58:46 by gde-jesu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,14 @@ int main()
 	PhoneBook	Contacts;
 	std::string	command;
 
-	while (command.compare("EXIT") || command.compare("exit"))
+	while (command.compare("EXIT"))
 	{
 		std::cout << "Enter the command (ADD, SEARCH or EXIT)";
 		getline(std::cin, command);
 		
-		if (!command.compare("ADD") || !command.compare("add"))
+		if (!command.compare("ADD"))
 			std::cout << "add contact" << std::endl;
-		else if (!command.compare("SEARCH") || !command.compare("search"))
+		else if (!command.compare("SEARCH"))
 			std::cout << "search contact" << std::endl;
 		else
 			std::cout << "Wrong command, please digit a valid command!" << std::endl;
